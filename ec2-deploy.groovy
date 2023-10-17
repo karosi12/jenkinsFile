2dd132c132c1f8a8a14d1046d15197101a4660d5
Adeyemi Kayode <adekayor@gmail.com>
2023-10-17 02:53:25 +0200

    feat: update agent id

--- ec2-deploy.groovy
+++ ec2-deploy.groovy
@@ -10,8 +10,8 @@ def deploy() {
   script {
     echo "Deploying the application"
     def dockerCmd = 'docker run -d -p 3080:3080 karosi12/demo-app:v1'
-    sshagent(['ec2-server-key']) {
-      sh "ssh -o StrictHostKeyChecking=no ec2-user@34.231.139.165 ${dockerCmd}"
+    sshagent(['70a5c83e-ed01-4df2-8a8f-8ef420f0f1e0']) {
+      sh "ssh -o StrictHostKeyChecking=no ec2-user@44.206.140.40 ${dockerCmd}"
     }
   }
 }
